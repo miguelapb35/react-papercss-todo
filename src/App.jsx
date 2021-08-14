be1f175f6f7612ceb4e689a54c8cc4ecc27b3f3f
Miguel <miguelapb35@gmail.com>
2021-08-14 14:46:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+
+const Container = styled.div`
+  background-color: #f1f1f1;
+  margin: 0;
+  min-height: 100vh;
+`;
+
+const Paper = styled.div`
+  width: 480px !important;
+  background-color: white;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <Container className="row flex-center flex-middle">
+        <Paper className="border border-primary padding-large margin-large no-responsive">
+          <h3 className="margin-small">Paper Todo</h3>
+          <AddTodo />
+          <TodoList />
+        </Paper>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// Reducer functions take state from Redux and action objects and
+// and returns a new state
+// Here, store has values of states of both todos and filter
+
+import { combineReducers } from "redux";
+import todos from "./todos";
+import filter from "./filter";
+
+export default combineReducers({
+  todos,
+  filter,
+});
